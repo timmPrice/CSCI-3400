d2417c28c5908c966f2812cd98fab0c426ad82cb
timmPrice <timmPrice6@gmail.com>
2024-09-08 22:18:43 -0400

    remove old files

--- c#/learn1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Classes;
-
-Console.WriteLine("The current time is " + DateTime.Now);
-
-var account = new BankAccount("Jane", 1000);
-Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance");

de1c10f4388eeee88126d2cc197ea131556c889d
timmPrice <timmPrice6@gmail.com>
2024-09-24 22:14:39 -0400

    adding and finishing pest6 - BSTrees

---
+++ pset6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication1;
+
+public class Program{
+	public static void Main(){
+		BinSearchTree<int> bstree = new BinSearchTree<int>{};
+		
+        for(int i = 0; i < 100; i++){
+			bstree.Insert(i);
+		}
+        
+        int depth = bstree.FindDepth();
+        Console.WriteLine("this is the depth " + depth);
+	}
+}
